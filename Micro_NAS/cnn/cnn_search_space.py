ea527d33491c16e63a4afbfc68079f9d4a295caf
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-10-02 16:42:56 +0800

    Add files via upload

---
+++ Micro_NAS/cnn/cnn_search_space.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from search_space import SearchSpace, ArchType, SchemaType
+from .cnn_morphisms import produce_all_morphs
+from .cnn_random_generators import random_arch
+from .cnn_schema import get_schema
+
+
+class CnnSearchSpace(SearchSpace):
+    input_shape = None
+    num_classes = None
+    # input_shape = (80, 80, 3)
+    # num_classes = 6
+
+
+
+
+    def __init__(self, dropout=0.0):
+        self.dropout = dropout
+
+    @property
+    def schema(self) -> SchemaType:
+        return get_schema()
+
+    def random_architecture(self) -> ArchType:
+        return random_arch()
+
+    def produce_morphs(self, arch: ArchType) -> List[ArchType]:
+        return produce_all_morphs(arch)
+
+    def to_keras_model(self, arch: ArchType, input_shape=None, num_classes=None, **kwargs):
+        input_shape = input_shape or self.input_shape
+        return arch.to_keras_model(input_shape=input_shape or self.input_shape,
+                                   num_classes=num_classes or self.num_classes,
+                                   dropout=self.dropout,
+                                   **kwargs)
